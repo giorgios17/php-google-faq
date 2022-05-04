d95bfd4bb84939bb1b4647fde9f163c69bc7d56c
giorgios17 <giosur@live.it>
2022-05-04 16:23:47 +0200

    font roboto

--- header.php
+++ header.php
@@ -4,7 +4,9 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	...
-	<title>Seventyone | Un nuovo sito targato WordPress</title>
+	<title>Domande frequenti - FAQ</title>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="main.css" />
 	<script src="/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
